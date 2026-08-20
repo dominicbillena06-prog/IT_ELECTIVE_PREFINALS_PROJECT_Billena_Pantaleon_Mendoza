f830e3477bbd0769fc51ad2830e8aa601935bd82
dominicbillena06-prog <dominicbillena06@gmail.com>
2026-08-21 00:32:06 +0800

    File creation

---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using IT_ELECTIVE_PREFINALS_PROJECT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IT_ELECTIVE_PREFINALS_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_ELECTIVE_PREFINALS_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
